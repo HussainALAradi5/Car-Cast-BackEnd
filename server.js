d40085cb7f3f87b12f678669e794c28d51992893
hussain <hussainaradi.ha@gmail.com>
2024-06-12 02:21:41 +0300

    work on the MVCR for user+car

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const userRouter = require('./routes/users')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
 const app = express()
+const methodOverride = require('method-override')
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json)
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(methodOverride('_method'))
 
 app.use('/', indexRouter)
 
