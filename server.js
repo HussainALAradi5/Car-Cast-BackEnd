149ddc30865643df3e69136afb72de2f3a0d4a61
hussain <hussainaradi.ha@gmail.com>
2024-06-12 00:31:16 +0300

    some work on the server

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const path = require('path')
 const carRouter = require('./routes/cars')
 const daySpellRouter = require('./routes/daySpells')
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const db = require('./db')
 const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json)
 app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-/* app.use('/cars', carRouter)
-app.use('/daySpells', daySpellRouter)
-app.use('/users', userRouter) */
 app.use('/', indexRouter)
 
 app.listen(PORT, () => {
