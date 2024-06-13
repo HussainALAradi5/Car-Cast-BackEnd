3e28ee0222fd86a6cf2c4bf54a4f3f3a43bcb400
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-06-13 12:41:26 +0300

    add a function in car.js routes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,14 +1,15 @@
 const Car = require('../models/car')
 
 async function create(req, res) {
-  const car = await Car.findById(req.params.id)
+  const carId = req.params.id
+  const car = await Car.findOne(carId)
   car.reviews.push(req.body)
   try {
     await car.save()
   } catch (err) {
     console.log(err)
   }
-  res.redirect(`/car/${car._id}`)
+  res.redirect(`/car/${car.carId}`)
 }
 
 module.exports = { create }
