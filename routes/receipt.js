fda83ac40e6e1614e78f826f7770ce8b3f2327d3
MaryamAlmutawa9 <mariamalmutawa5@gmail.com>
2024-06-17 15:16:26 +0300

    pull

--- routes/receipt.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const receiptsCtrl = require("../controllers/receipts")
-
-router.get("/cars/:id/receipts", receiptsCtrl.show)
-
-router.post("/cars/:id/receipts", receiptsCtrl.create)
-
-module.exports = router
