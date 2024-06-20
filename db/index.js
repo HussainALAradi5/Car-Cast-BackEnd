1199815f0e0d25b48e19f016ec882c7a047342f7
hussain <hussainaradi.ha@gmail.com>
2024-06-20 15:07:23 +0300

    some minor fix

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((e) => {
-    console.error('Connection error', e.message)
-  })
+mongoose.set('strictQuery', false)
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI)
+  } catch (error) {
+    console.error('Connection error', error.message)
+  }
+}
+connect()
 const db = mongoose.connection
 module.exports = db
